1970c68f28b07d1cea91eb0f8bee4393dbf31c23
automatealchemist <ujjwalsingh0407@gmail.com>
2025-01-06 20:41:02 +0530

    confitest & utilities package added.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -10,9 +12,23 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-@pytest.fixture()
-def setup():
+@pytest.fixture(scope="class")
+def setup(request):
     chrome_option = Options()
     chrome_option.add_experimental_option("detach", True)
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_option)
     driver.maximize_window()
+    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    time.sleep(5)
+
+    driver.find_element(By.XPATH,
+                        '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input').send_keys(
+        "Admin")
+    driver.find_element(By.XPATH,
+                        '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input').send_keys(
+        "admin123")
+
+    driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button').click()
+    request.cls.driver=driver
+    yield
+    driver.close()
