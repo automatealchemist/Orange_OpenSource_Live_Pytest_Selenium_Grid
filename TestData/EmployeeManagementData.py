88c02e6de4fb24579bcc08887131f5f29ef6e670
automatealchemist <ujjwalsingh0407@gmail.com>
2025-01-14 08:40:21 +0530

    Code for test data added.

---
+++ TestData/EmployeeManagementData.py
@@ -0,0 +1,15 @@
+import random
+
+
+class EmployeeManagementData:
+    test_employee_data = [
+        {
+            "firstname": "Angry",
+            "middlename": "k",
+            "lastname": "Bird",
+            "username": "angrybird",
+            "password": "Password@123",
+            "confirmpassword": "Password@123",
+
+        }
+    ]
