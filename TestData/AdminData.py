816ed97205530dd82749b0a2327f3d66e96318b5
automatealchemist <ujjwalsingh0407@gmail.com>
2025-01-14 08:40:21 +0530

    Code for test data added.

---
+++ TestData/AdminData.py
@@ -0,0 +1,9 @@
+class AdminData:
+    test_admin_data =[
+        {
+            "name" :"Angry K Bird",
+            "password": "Password@123",
+            "confirmpassword": "Password@123"
+
+        }
+    ]
---
+++ TestData/DeleteEmployeeData.py
@@ -0,0 +1,8 @@
+
+
+class DeleteEmployeeData:
+    test_delete_employee_data =[
+        {
+            "name" : "Angry K Bird"
+        }
+    ]
