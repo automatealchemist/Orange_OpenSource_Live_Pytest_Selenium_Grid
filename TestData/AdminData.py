60ffb0e1ea33d4f38a2034605f18be4ab5a79864
automatealchemist <ujjwalsingh0407@gmail.com>
2025-03-23 18:54:34 +0530

    Selenium grid code added.

--- TestData/AdminData.py
+++ TestData/AdminData.py
@@ -1,9 +1,21 @@
+from TestData.EmployeeManagementData import EmployeeManagementData
+
 class AdminData:
-    test_admin_data =[
+    # Fetch name_data once
+    name_data = EmployeeManagementData.name_data
+
+    # Extract first, middle, and last name safely
+    first_name = name_data.get("first_name", "")
+    middle_name = name_data.get("middle_name", "")
+    last_name = name_data.get("last_name", "")
+
+    # Ensure spaces while constructing the full name
+    full_name = f"{first_name} {middle_name} {last_name}".strip()
+
+    test_admin_data = [
         {
-            "name" :"Angry K Bird",
+            "name": full_name,  # Use the corrected full_name
             "password": "Password@123",
             "confirmpassword": "Password@123"
-
         }
     ]
--- pageObjects/Admin.py
+++ pageObjects/Admin.py
@@ -17,16 +17,7 @@ class Admin:
     password = (By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[2]/div/div[1]/div/div[2]/input')
     confirm_password = (By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[2]/div/div[2]/div/div[2]/input')
     data_saves = (By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[3]/button[2]')
-    #emp_search = (By.XPATH,'//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/input')
-    #search_b = (By.XPATH,'//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[2]')
-    #delete = (By.XPATH,'//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div[6]/div/button[1]/i')
-    #delete_c = (By.XPATH, '//*[@id="app"]/div[3]/div/div/div/div[3]/button[2]')
-    #pim_p = (By.XPATH,'//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a')
-    #search_name = (By.XPATH,'//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/div/div/input')
-    #num = (By.XPATH,'//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[2]/div/div[2]/input')
-    #search_pim  = (By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[2]')
-    #delete_pim = (By.XPATH,'//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div[9]/div/button[2]')
-    #delete_con = (By.XPATH,'//*[@id="app"]/div[3]/div/div/div/div[3]/button[2]')
+
     admin = (By.XPATH, '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a')
 
 
